ece40c357f090e2a37258236be238ed3001dbabd
joshgressman <joshgressman@gmail.com>
2018-02-18 17:12:09 -0600

    added firebase loggin function

--- Flash Chat/LogInViewController.swift
+++ Flash Chat/LogInViewController.swift
@@ -6,7 +6,7 @@
 
 
 import UIKit
-
+import Firebase
 
 class LogInViewController: UIViewController {
 
@@ -28,7 +28,15 @@ class LogInViewController: UIViewController {
 
         
         //TODO: Log in the user
-        
+        Auth.auth().signIn(withEmail: emailTextfield.text!, password: passwordTextfield.text!) { (user, error) in
+           
+            if error != nil {
+                print("There was an error logging in")
+            } else {
+                print("Loggin Successful!!")
+                self.performSegue(withIdentifier: "goToChat", sender: self)
+            }
+        }
         
     }
     
